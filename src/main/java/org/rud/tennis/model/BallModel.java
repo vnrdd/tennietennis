2c3a87ba83eb9de2679b30659eed23daf573406a
vnrdd <56083351+vnrdd@users.noreply.github.com>
2020-12-22 10:05:52 +0300

    Squash Mode finished

--- src/main/java/org/rud/tennis/model/BallModel.java
+++ src/main/java/org/rud/tennis/model/BallModel.java
@@ -1,13 +1,16 @@
 package org.rud.tennis.model;
 
+import org.rud.tennis.view.states.SquashState;
+
 import java.awt.*;
+import java.util.Random;
 
 public class BallModel {
     private int x;
     private int y;
 
-    public int xSpeed = 0;
-    public int ySpeed = 0;
+    private int xSpeed = 2;
+    private int ySpeed = 0;
 
     private Rectangle hitBox;
 
@@ -15,6 +18,8 @@ public class BallModel {
         this.x = x;
         this.y = y;
         hitBox = new Rectangle(x, y, WIDTH, HEIGHT);
+        while(ySpeed == 0)
+            ySpeed = (new Random()).nextInt(6) - 3;
     }
 
     public int getX(){
@@ -25,7 +30,28 @@ public class BallModel {
         return y;
     }
 
-    public void set(){
+    public void set(SquashState pitch){
+        if(hitBox.intersects(pitch.getPlayer().getModel().getHitBox())) {
+            xSpeed = -xSpeed;
+            pitch.score++;
+        }
+        for(int i = 0; i < pitch.getWalls().size(); ++i){
+            if(hitBox.intersects(pitch.getWalls().get(i).getModel().getHitBox())) {
+                if(i == 0 || i == 1)
+                    ySpeed = -ySpeed;
+                else
+                    xSpeed = -xSpeed;
+            }
+        }
+        if(hitBox.intersects(pitch.getGoalBorder().getModel().getHitBox())) {
+            xSpeed = 0;
+            ySpeed = 0;
+            pitch.gameOver = true;
+        }
+        x += xSpeed;
+        y += ySpeed;
+        hitBox.x += xSpeed;
+        hitBox.y += ySpeed;
     }
 
     public Rectangle getHitBox(){
--- src/main/java/org/rud/tennis/view/states/SquashState.java
+++ src/main/java/org/rud/tennis/view/states/SquashState.java
@@ -6,7 +6,6 @@ import org.rud.tennis.view.objects.*;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class SquashState extends GameState{
@@ -15,7 +14,8 @@ public class SquashState extends GameState{
     private Border goalBorder;
     private ArrayList<Border> walls;
     private Player player;
-    private int score = 0;
+    public int score = 0;
+    public boolean gameOver = false;
 
     public SquashState(GameStateManager gsm){
         this.gsm = gsm;
@@ -39,6 +39,7 @@ public class SquashState extends GameState{
 
     public void update() {
         player.getModel().set();
+        ball.getModel().set(this);
     }
 
     public void draw(Graphics2D g) {
@@ -51,11 +52,35 @@ public class SquashState extends GameState{
         goalBorder.draw(g);
         for(Border b : walls)
             b.draw(g);
+        if(gameOver){
+            bg.draw(g);
+            g.setColor(new Color(252, 163, 17));
+            g.setFont(new Font("TT Hoves DemiBold", Font.PLAIN, 50));
+            g.drawString("Your score: " + String.valueOf(score), 330, 200);
+
+            g.setColor(new Color(219, 223, 225));
+            g.setFont(new Font("TT Hoves DemiBold", Font.PLAIN, 30));
+            g.drawString("Press ESC to leave", 330, 500);
+        }
+    }
+
+    public ArrayList<Border> getWalls(){
+        return walls;
+    }
+
+    public Border getGoalBorder(){
+        return goalBorder;
+    }
+
+    public Player getPlayer(){
+        return player;
     }
 
     public void keyPressed(int k) {
-        if(k == KeyEvent.VK_ESCAPE)
+        if(k == KeyEvent.VK_ESCAPE) {
+            reset();
             gsm.setState(GameStateManager.MENUSTATE);
+        }
         if(k == KeyEvent.VK_UP) {
             player.getModel().ySpeed = -2;
         }
@@ -67,5 +92,17 @@ public class SquashState extends GameState{
     public void keyReleased(int k) {
         if(k == KeyEvent.VK_UP || k == KeyEvent.VK_DOWN)
             player.getModel().ySpeed = 0;
+    }
+
+    private void reset(){
+        player = new Player(118, 292);
+        ball = new Ball(500, 353);
+        goalBorder = new Border(30, 48, 12, 612, "/goalBorder.png");
+        walls = new ArrayList<Border>();
+        walls.add(new Border(30, 37, 924, 12, "/wall.png"));
+        walls.add(new Border(30, 660, 924, 12, "/wall.png"));
+        walls.add(new Border(953, 37, 12, 635, "/wall.png"));
+        gameOver = false;
+        score = 0;
     }
 }
